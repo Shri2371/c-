57582883c0058569c2683766263ecc9a3cdf65de
Shriniwas mane <118557015+Shri2371@users.noreply.github.com>
2023-04-09 04:33:56 -0700

    Add files via upload

---
+++ loops.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{   
+    int n,k=0,i;
+    cout<<"Enter the square = ";
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+        k=i*i;
+    }
+    cout<<k;
+    return 0;
+
+}
